e2d288ba5667488944a2aaf19030591d9f6297b3
nataliacarrera <104646501+nataliacarrera@users.noreply.github.com>
2022-11-24 11:43:28 -0300

    added operations enum and bases for all calculations

--- src/screens/calculator/Calculator.screen.tsx
+++ src/screens/calculator/Calculator.screen.tsx
@@ -1,20 +1,48 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Text, View} from 'react-native';
 import {theme} from '../../theme/theme';
 import {styles} from '../Calculator.styles';
 import {Button} from '../../components/button/Button.component';
 
+enum Operations {
+  'addition',
+  'subtraction',
+  'multiplication',
+  'division',
+}
+
 export const Calculatorscreen = () => {
   const [number, setNumber] = useState('0');
   const [previousNumber, setPreviousNumber] = useState('0');
 
+  const currentOperation = useRef<Operations>();
   const wipeCalculations = () => {
     setNumber('0');
+    setPreviousNumber('0');
   };
 
   const buildNumber = (textNumber: string) => {
+    //Reject double '.'
     if (number.includes('.') && textNumber === '.') return;
-    // setNumber(number + textNumber);
+
+    if (number.startsWith('0') || number.startsWith('-0')) {
+      //decimal
+      if (textNumber === '.') {
+        setNumber(number + textNumber);
+        // If different from '0' and has decimals already
+      } else if (textNumber === '0' && number.includes('.')) {
+        setNumber(number + textNumber);
+        // If different from '0' and doesn't have decimal
+      } else if (textNumber !== '0' && !number.includes('.')) {
+        setNumber(textNumber);
+      } else if (textNumber === '0' && !number.includes('.')) {
+        setNumber(number);
+      } else {
+        setNumber(number);
+      }
+    } else {
+      setNumber(number + textNumber);
+    }
   };
 
   const negativePositiveToggle = () => {
@@ -24,11 +52,49 @@ export const Calculatorscreen = () => {
       setNumber('-' + number);
     }
   };
+
+  const delBtn = () => {
+    if ((number.length === 2 && number.includes('-')) || number.length === 1) {
+      setNumber('0');
+    } else {
+      setNumber(number.slice(0, -1));
+    }
+  };
+
+  const resolvePreviousNumber = () => {
+    if (number.endsWith('.')) {
+      setPreviousNumber(number.slice(0, -1));
+    } else {
+      setPreviousNumber(number);
+    }
+    setNumber('0');
+  };
+
+  const divisionBtn = () => {
+    resolvePreviousNumber();
+    currentOperation.current = Operations.division;
+  };
+
+  const multiplicationBtn = () => {
+    resolvePreviousNumber();
+    currentOperation.current = Operations.multiplication;
+  };
+  const substractionBtn = () => {
+    resolvePreviousNumber();
+    currentOperation.current = Operations.subtraction;
+  };
+  const additionBtn = () => {
+    resolvePreviousNumber();
+    currentOperation.current = Operations.addition;
+  };
   return (
     <View style={styles.container}>
-      <Text style={[theme.textDefault, styles.smallResultText]}>
-        {previousNumber}
-      </Text>
+      {previousNumber !== '0' && (
+        <Text style={[theme.textDefault, styles.smallResultText]}>
+          {previousNumber}
+        </Text>
+      )}
+
       <Text
         style={[theme.textDefault, styles.resultText]}
         numberOfLines={1}
@@ -47,16 +113,8 @@ export const Calculatorscreen = () => {
           backgroundColor="#9B9B9B"
           onPress={negativePositiveToggle}
         />
-        <Button
-          content="del"
-          backgroundColor="#9B9B9B"
-          onPress={wipeCalculations}
-        />
-        <Button
-          content="/"
-          backgroundColor="orange"
-          onPress={wipeCalculations}
-        />
+        <Button content="del" backgroundColor="#9B9B9B" onPress={delBtn} />
+        <Button content="/" backgroundColor="orange" onPress={divisionBtn} />
       </View>
       <View style={styles.row}>
         <Button content="7" onPress={buildNumber} />
@@ -65,7 +123,7 @@ export const Calculatorscreen = () => {
         <Button
           content="X"
           backgroundColor="orange"
-          onPress={wipeCalculations}
+          onPress={multiplicationBtn}
         />
       </View>
       <View style={styles.row}>
@@ -75,18 +133,14 @@ export const Calculatorscreen = () => {
         <Button
           content="-"
           backgroundColor="orange"
-          onPress={wipeCalculations}
+          onPress={substractionBtn}
         />
       </View>
       <View style={styles.row}>
         <Button content="1" onPress={buildNumber} />
         <Button content="2" onPress={buildNumber} />
         <Button content="3" onPress={buildNumber} />
-        <Button
-          content="+"
-          backgroundColor="orange"
-          onPress={wipeCalculations}
-        />
+        <Button content="+" backgroundColor="orange" onPress={additionBtn} />
       </View>
       <View style={styles.row}>
         <Button content="0" twoRowButton onPress={buildNumber} />
